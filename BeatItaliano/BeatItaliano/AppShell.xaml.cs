abb5a64b800b26a27e979177f89dda705d763293
simonedeitos <info@simonedeitos.it>
2026-04-11 01:20:33 +0200

    up

---
+++ BeatItaliano/BeatItaliano/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BeatItaliano
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BeatItaliano/BeatItaliano/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using LibVLCSharp.MAUI;
+using Microsoft.Extensions.Logging;
+
+namespace BeatItaliano;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseLibVLCSharp()  // <-- Registra LibVLCSharp
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
